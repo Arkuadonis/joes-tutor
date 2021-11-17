bd1bd6418047f8969ffa80c2d6e0dc4f96f9b21c
Arkuadonis <51827906+Arkuadonis@users.noreply.github.com>
2021-11-17 16:17:38 +0530

    Operators

---
+++ Comparison.py
@@ -0,0 +1,11 @@
+a = 10
+b = 10
+
+print("A =",a ,"B =",b)
+
+print("A == B", a == b)
+print("A != B", a != b)
+print("A <  B", a < b)
+print("A >  B", a > b)
+print("A <= B", a <= b)
+print("A >= B", a >= b)
---
+++ arithmetic.py
@@ -0,0 +1,39 @@
+
+# Arithmetic Operator
+
+a = 123
+b = 10
+
+print("A=",a)
+print("B=",b)
+
+print("A + B = ",a+b)
+print("A - B = ",a-b)
+print("A * B = ",a*b)
+print("A / B = ",a/b)
+print("A % B = ",a%b)
+print("A // B = ",a//b)
+print("A ** B = ",a**b)
+
+# Assignment Arithmetic operator
+
+print("Arithmetic Assignment")
+
+a+=b
+print(a)
+a-=b
+print(a)
+a*=b
+print(a)
+a/=b
+print(a)
+a%=b
+print(a)
+
+a=123
+b=10
+
+a//=b
+print(a)
+a**=b
+print(a)
